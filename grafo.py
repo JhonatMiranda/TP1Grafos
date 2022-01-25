92a7ff82230143da72baaa077142ceb2beda3369
Jhonata <50640451+JhonatMiranda@users.noreply.github.com>
2022-01-25 13:18:28 -0300

    up

--- grafo.py
+++ grafo.py
@@ -3,7 +3,12 @@ qtvertices=arq.readline()
 qtvertices= int(qtvertices)
 linhas=arq.readlines()
 matadj=[[0 for j in range(qtvertices)]for i in range(qtvertices)]
-
+for i in linhas:
+    posx=int(i[0])
+    posy=int(i[2])
+    matadj[posx-1][posy-1]=1
+    matadj[posy-1][posx-1]=1
+print(matadj)
 
 
 
