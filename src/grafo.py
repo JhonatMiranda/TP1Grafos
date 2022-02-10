750803875a0eb71faf725f791501656a683f688a
Jhonata <50640451+JhonatMiranda@users.noreply.github.com>
2022-02-10 18:43:31 -0300

    up

--- src/grafo.py
+++ src/grafo.py
@@ -7,6 +7,92 @@ from collections import defaultdict
 from ipython_genutils.py3compat import xrange
 
 
+class Heap():
+
+    def __init__(self):
+        self.array = []
+        self.size = 0
+        self.pos = []
+
+    def newMinHeapNode(self, v, dist):
+        minHeapNode = [v, dist]
+        return minHeapNode
+
+    def swapMinHeapNode(self, a, b):
+        t = self.array[a]
+        self.array[a] = self.array[b]
+        self.array[b] = t
+
+    def minHeapify(self, idx):
+        smallest = idx
+        left = 2 * idx + 1
+        right = 2 * idx + 2
+
+        if left < self.size and self.array[left][1] < \
+                self.array[smallest][1]:
+            smallest = left
+
+        if right < self.size and self.array[right][1] < \
+                self.array[smallest][1]:
+            smallest = right
+
+        if smallest != idx:
+            self.pos[self.array[smallest][0]] = idx
+            self.pos[self.array[idx][0]] = smallest
+
+            # Swap nodes
+            self.swapMinHeapNode(smallest, idx)
+
+            self.minHeapify(smallest)
+
+    def extractMin(self):
+
+        if self.isEmpty() == True:
+            return
+
+        root = self.array[0]
+
+        lastNode = self.array[self.size - 1]
+        self.array[0] = lastNode
+
+        self.pos[lastNode[0]] = 0
+        self.pos[root[0]] = self.size - 1
+
+        self.size -= 1
+        self.minHeapify(0)
+
+        return root
+
+    def isEmpty(self):
+        return True if self.size == 0 else False
+
+    def decreaseKey(self, v, dist):
+
+
+        i = self.pos[v]
+
+        self.array[i][1] = dist
+
+        while i > 0 and self.array[i][1] < \
+                self.array[(i - 1) // 2][1]:
+            self.pos[self.array[i][0]] = (i - 1) / 2
+            self.pos[self.array[(i - 1) // 2][0]] = i
+            self.swapMinHeapNode(i, (i - 1) // 2)
+
+            i = (i - 1) // 2;
+
+    def isInMinHeap(self, v):
+
+        if self.pos[v] < self.size:
+            return True
+        return False
+
+
+def printArr(parent, n):
+    for i in range(1, n):
+        print("% d - % d" % (parent[i], i))
+
+
 class VerticeAdj():
     def __init__(self, vertice, weight):
         self.vertice = vertice
@@ -51,15 +137,16 @@ class Graph():
         return self.tamanho() / (self.V - 1)
 
     def vizinhos(self, vertice_escolhido):
+        vizinhos=[]
         encontrado = False
         for i in range(self.V):
             if i == vertice_escolhido:
                 encontrado = True
                 aux = self.graph[i]
                 while aux:
-                    print("O vértice", aux.vertice, "é vizinho do vértice", i)
+                    vizinhos.append(aux.vertice)
                     aux = aux.next
-
+        return vizinhos
     def grau(self, vertice_escolhido):
         grau = 0
         for i in range(self.V):
@@ -219,34 +306,44 @@ class Graph():
                         return True
             return False
 
+    def PrimMST(self):
+        V = self.V
 
-    # def printArr(self, dist):
-    #     print("Vertex Distance from Source")
-    #     for i in range(self.V -1):
-    #         print("{0}\t\t{1}".format(i+1, dist[i]))
+        key = []
 
-            
-    # def BellmanFord(self, src):
-    #     dist = [float("Inf")] * (self.V -1)
-    #     dist[src] = 0
-        
-    #     for _ in range(self.V):
-    #         aux = self.graph[src]
-    #         while aux:
-    #             u = aux.vertice
-    #             v = aux.next.vertice
-    #             if dist[u] != float("Inf") and dist[u] + aux.next.weight < dist[v]:
-    #                 dist[v] = dist[u] + aux.next.weight
-    #             aux = aux.next
+        parent = []
 
-    #     aux = self.graph[src]
-    #     while aux:
-    #         u = aux.vertice
-    #         v = aux.next.vertice
-    #         if dist[u] != float("Inf") and dist[u] + aux.next.weight < dist[v]:
-    #             print("Negative weight cycle exists")
-    #             return
-    #     self.printArr(dist)
+        minHeap = Heap()
+
+        for v in range(V):
+            parent.append(-1)
+            key.append(1e7)
+            minHeap.array.append(minHeap.newMinHeapNode(v, key[v]))
+            minHeap.pos.append(v)
+
+        minHeap.pos[0] = 0
+        key[0] = 0
+        minHeap.decreaseKey(0, key[0])
+
+        minHeap.size = V;
+
+        while minHeap.isEmpty() == False:
+
+            newHeapNode = minHeap.extractMin()
+            u = newHeapNode[0]
+
+            for pCrawl in self.graph[u]:
+
+                v = pCrawl[0]
+
+                if minHeap.isInMinHeap(v) and pCrawl[1] < key[v]:
+                    key[v] = pCrawl[1]
+                    parent[v] = u
+
+                    minHeap.decreaseKey(v, key[v])
+
+        printArr(parent, V)
+
 # ----------------------------------------------------- --------------------------------------------------#
 if __name__ == "__main__":
 
@@ -276,4 +373,4 @@ if __name__ == "__main__":
     #     print("Grafo com ciclo")
     # else:
     #     print("Grafo sem ciclo")
-    # matpes.BellmanFord(1)
+    matpes.PrimMST()
